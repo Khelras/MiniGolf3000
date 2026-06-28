4acb19714a84f79cb38dcf386b29cc3a3c9f297e
AnJello <anjello.30bohol@gmail.com>
2026-06-29 11:38:32 +1200

    Player Movement Relative to the Camera
    All Player Movement is now relative to the Camera for natural movement controls. The movement calculations uses the Camera's forward and right vector to ensure the Player move relative to the direction the Camera is facing towards.

--- scripts/CameraController.cs
+++ scripts/CameraController.cs
@@ -3,16 +3,14 @@ using System;
 
 public partial class CameraController : SpringArm3D
 {
-	[Export] private Camera3D camera;
-	[Export(PropertyHint.Range, "0.01,1,0.01")] private float mouseSensitivity = 0.5f;
+	[Export] private Camera3D _camera;
+	[Export(PropertyHint.Range, "0.01,1,0.01")] private float _mouseSensitivity = 0.5f;
 	
-	Vector2 cameraInputDirection = Vector2.Zero;
+	private Vector2 _cameraInputDirection = Vector2.Zero;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		if (this.camera == null) GD.PushError("Camera reference was not set");
-		
 		// Set the Mouse Input Mode to Capture
 		Input.SetMouseMode(Input.MouseModeEnum.Captured);
 	}
@@ -22,16 +20,20 @@ public partial class CameraController : SpringArm3D
 	{
 		// Ensure that the Mouse Input Mode is set to Capture
 		if (Input.GetMouseMode() != Input.MouseModeEnum.Captured) return;
-		
+			
 		// Camera Movement Input Rotations
 		Vector3 cameraRotation = this.Rotation;
-		cameraRotation.X -= this.cameraInputDirection.Y * (float)delta; // Pitch
-		cameraRotation.X = Mathf.Clamp(cameraRotation.X, -Single.Pi / 2f, Single.Pi / 2f);
-		cameraRotation.Y -= this.cameraInputDirection.X * (float)delta; // Yaw
-		this.cameraInputDirection = Vector2.Zero;
+		cameraRotation.X -= this._cameraInputDirection.Y * (float)delta; // Pitch
+		cameraRotation.Y -= this._cameraInputDirection.X * (float)delta; // Yaw
+		
+		// Min and Max Pitch Rotations to -+89 (because 90 causes Backward Movement)
+		float minPitchRotation = (-89f * Single.Pi) / 180f; // -89 Degrees
+		float maxPitchRotation = (89f * Single.Pi) / 180f; // 89 Degrees
+		cameraRotation.X = Mathf.Clamp(cameraRotation.X, minPitchRotation, maxPitchRotation);
 		
 		// Apply the Camera Movement
 		this.Rotation = cameraRotation;
+		this._cameraInputDirection = Vector2.Zero; // Reset the Camera Input Direction
 	}
 	
 	public override void _Input(InputEvent @event)
@@ -40,10 +42,10 @@ public partial class CameraController : SpringArm3D
 		if (@event is InputEventKey key)
 		{
 			// Key was Pressed
-			if (key.Pressed == true)
+			if (key.Pressed)
 			{
 				// Quit Game
-				if (key.IsActionPressed("QuitGame") == true)
+				if (key.IsActionPressed("QuitGame"))
 				{
 					this.GetTree().Quit();
 				}
@@ -60,7 +62,7 @@ public partial class CameraController : SpringArm3D
 			if (Input.GetMouseMode() != Input.MouseModeEnum.Captured) return;
 			
 			// Camera Movement Input
-			this.cameraInputDirection = mouseMotion.ScreenRelative * this.mouseSensitivity;
+			this._cameraInputDirection = mouseMotion.ScreenRelative * this._mouseSensitivity;
 		}
 	}
 }
--- scripts/PlayerController.cs
+++ scripts/PlayerController.cs
@@ -1,43 +1,76 @@
 using Godot;
-using System;
 
 public partial class PlayerController : CharacterBody3D
 {
-	public const float Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
+	[ExportCategory("Movement")]
+	[Export] public float Speed = 5f;
+	[Export] public float Acceleration = 20f;
+	[Export] public float JumpHeight = 4f;
 
+	[ExportCategory("Camera")]
+	[Export] private Camera3D _camera;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// Ensure the Speed Property was Set Properly
+		if (Speed <= 0f)
+		{
+			GD.PushWarning("Speed set incorrectly, defaulting to 5.");
+			this.Speed = 5f; // Default to 5
+		}
+		
+		// Ensure the Acceleration Property was Set Properly
+		if (Acceleration <= 0f)
+		{
+			GD.PushWarning("Acceleration set incorrectly, defaulting to 20.");
+			this.Acceleration = 20f;
+		}
+		
+		// Ensure the Jump Height Property was Set Properly
+		if (this.JumpHeight <= 0f)
+		{
+			GD.PushWarning("Jump height set incorrectly, defaulting to 4.");
+			this.JumpHeight = 4f;
+		}
+		
+		// Set the Mouse Input Mode to Capture
+		Input.SetMouseMode(Input.MouseModeEnum.Captured);
+	}
+	
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+	
 	public override void _PhysicsProcess(double delta)
 	{
-		Vector3 velocity = Velocity;
+		// 3D Velocity Vector
+		Vector3 velocity = this.Velocity;
 
-		// Add the gravity.
-		if (!IsOnFloor())
-		{
-			velocity += GetGravity() * (float)delta;
-		}
-
-		// Handle Jump.
-		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
-		{
-			velocity.Y = JumpVelocity;
-		}
-
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 inputDir = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
-		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
-		if (direction != Vector3.Zero)
-		{
-			velocity.X = direction.X * Speed;
-			velocity.Z = direction.Z * Speed;
-		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
-			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
-		}
-
-		Velocity = velocity;
+		// Apply Gravity if the Player is not on the Floor
+		if (!this.IsOnFloor()) velocity += this.GetGravity() * (float)delta;
+		
+		// Apply the Jump Height when the Player Jumps and was on the Floor
+		if (Input.IsActionJustPressed("Jump") && this.IsOnFloor()) velocity.Y += this.JumpHeight;
+		
+		// Use the Camera's Forward and Right Vector for Movement to be Relative to the Camera
+		Vector3 forward = this._camera.GlobalBasis.Z;
+		Vector3 right = this._camera.GlobalBasis.X;
+		
+		// Get the Input Axis and apply the Forward and Right Vector for Movement Direction
+		Vector2 inputAxis = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+		Vector3 movementDirection = (forward * inputAxis.Y) + (right * inputAxis.X);
+		movementDirection.Y = 0f;
+		movementDirection = movementDirection.Normalized();
+		
+		// Calculate the Velocity based on the Movement Direction and Speed
+		velocity.X = Mathf.MoveToward(this.Velocity.X, movementDirection.X * this.Speed, this.Acceleration * (float)delta);
+		velocity.Z = Mathf.MoveToward(this.Velocity.Z, movementDirection.Z * this.Speed, this.Acceleration * (float)delta);
+		
+		// Apply the Velocity
+		this.Velocity = velocity;
 		MoveAndSlide();
 	}
 }
